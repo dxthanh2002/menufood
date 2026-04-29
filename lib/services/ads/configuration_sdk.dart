f2b92722c6837b5d34c4f171b6d83184ed0aa9cb
dung pham <pad2462003@gmail.com>
2026-04-29 15:18:48 +0700

    change configuration

--- lib/services/ads/configuration_sdk.dart
+++ lib/services/ads/configuration_sdk.dart
@@ -1,28 +1,27 @@
 import 'dart:io';
 
 class Configuration {
-  // static String appidStr = Platform.isIOS ? 'h69e5addb0b438' : 'h69e0f1cbe41bf';
+  static String appidStr = Platform.isIOS ? 'h69f1bbcac7665' : 'h69f1ba518fbf9';
 
-  // static String appidkeyStr = Platform.isIOS
-  //     ? 'ae69c40ea7a710759685bc53b15c796e8'
-  //     : 'ad688a334ae7556a1d8ed624f768c7fc9';
+  static String appidkeyStr = Platform.isIOS
+      ? 'a0a15737b3015889456b5679439578f18'
+      : 'aaa08828f483a4845ca4c135130e8149c';
 
-  // // we are using
-  // static String autoRewarderPlacementID = Platform.isIOS
-  //     ? 'n69e5aed45253b'
-  //     : 'n69e0f4102e66a';
+  // we are using
+  static String autoRewarderPlacementID = Platform.isIOS
+      ? 'n69f1bbfa212bc'
+      : 'n69f1ba8d9e51a';
 
-  // static String interstitial = Platform.isIOS
-  //     ? 'n69e5adf75ba4c'
-  //     : 'n69e0f2046d569';
+  static String interstitial = Platform.isIOS
+      ? 'n69f1bbdd40616'
+      : 'n69f1ba77ae0b5';
 
   // not use
-  static String appidStr = 'h69c34ae64bc55';
-  static String appidkeyStr = 'a3444276b691cc43de2178f797f33389c';
+  // static String appidStr = 'h69c34ae64bc55';
+  // static String appidkeyStr = 'a3444276b691cc43de2178f797f33389c';
 
-  static String autoRewarderPlacementID = 'n69c34b203beef';
-  static String rewarderTest = 'n69c34b203beef';
-  static String interstitial = 'n69c34b1fb07c2';
+  // static String autoRewarderPlacementID = 'n69c34b203beef';
+  // static String interstitial = 'n69c34b1fb07c2';
 
   static String rewarderPlacementID = Platform.isIOS
       ? 'n69c34b203beef'
